ab3f705e57fe04f93a1fde6673079398ffc10204
葛欣 <382599512@qq.com>
2019-08-30 17:00:01 +0800

    增加ios页面

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,21 @@ router.get("/android-wx", async (ctx, next) => {
 
   const WX_BrowerReg = /MicroMessenger/i;
   const androidPhone = /Android/i;
-  //如果是安卓微信浏览器 就返回一个文件
+
   if (WX_BrowerReg.test(userAgent) && androidPhone.test(userAgent)) {
+    //如果是安卓微信浏览器 就返回一个文件
     ctx.set({
       "Content-Disposition": "attachment;filename=river.apk"
     });
     ctx.response.status = 206;
     ctx.body = "";
   } else {
-    await ctx.render("demo", {
-      title: "river test 微信浏览打开默认浏览器"
+    await ctx.render("ios-user-guide", {
+      title: "ios用户打开浏览器引导"
     });
+    // await ctx.render("demo", {
+    //   title: "river test 微信浏览打开默认浏览器"
+    // });
   }
 });
 module.exports = router;
