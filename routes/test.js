1a843bad169ab3ca0514f5b307a645ee677dd808
river <382599512@qq.com>
2019-09-01 20:46:23 +0800

    测试下载 修改下载路由

--- routes/test.js
+++ routes/test.js
@@ -1,7 +1,11 @@
 const router = require("koa-router")();
 const send = require("koa-send");
 const path = require("path");
-router.get("/test", async (ctx, next) => {
+// 流的方式下载
+router.get("/download", async (ctx, next) => {
+  ctx.set({
+    "Content-Disposition": "attachment;filename=river.apk"
+  });
   await send(ctx, path.resolve("/public/assets/1.apk"));
 });
 
