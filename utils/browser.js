b39e81bdd49384d09f5295e4b55f4441d1581e73
river <382599512@qq.com>
2019-09-01 21:39:20 +0800

    fix

--- utils/browser.js
+++ utils/browser.js
@@ -3,23 +3,31 @@ class Browser {
     this.agent = agent;
     //浏览器类别
     this.WX_ANDROID = `WX_ANDROID`;
+    this.QQ_ANDROID = `QQ_ANDROID`;
     this.WX_IOS = `WX_IOS`;
     this.OTHER = `OTHER`;
   }
   getType() {
-    const { agent, WX_ANDROID, OTHER, WX_IOS } = this;
+    const { agent, WX_ANDROID, OTHER, WX_IOS, QQ_ANDROID } = this;
     const WX_BrowerReg = /MicroMessenger/i;
+    const QQ_BrowerReg = /MQQBrowser/i;
     const androidReg = /Android/i;
     const iosReg = /iPhone/i;
+    const isAndroid = agent => androidReg.test(agent);
+    const isIos = agent => iosReg.test(agent);
     while (true) {
+      if (QQ_BrowerReg.test(agent)) {
+        if (isAndroid(agent)) {
+          return QQ_ANDROID;
+        }
+      }
       if (!WX_BrowerReg.test(agent)) {
-        return OTHER;
-      }
-      if (androidReg.test(agent)) {
-        return WX_ANDROID;
-      }
-      if (iosReg.test(agent)) {
-        return WX_IOS;
+        if (isAndroid(agent)) {
+          return WX_ANDROID;
+        }
+        if (isIos(agent)) {
+          return WX_IOS;
+        }
       }
       return OTHER;
     }
