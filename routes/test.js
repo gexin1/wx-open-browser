73fcbb34bbc95044cecc2c11ec4dd88375d82df8
river <382599512@qq.com>
2019-09-01 21:17:20 +0800

    fix

--- routes/test.js
+++ routes/test.js
@@ -3,10 +3,7 @@ const send = require("koa-send");
 const path = require("path");
 // 流的方式下载
 router.get("/download", async (ctx, next) => {
-  ctx.set({
-    "Content-Disposition": "attachment;filename=1.png"
-  });
-  await send(ctx, path.resolve("/public/assets/qrcode.png"));
+  await send(ctx, path.resolve("/public/assets/test.apk"));
 });
 
 module.exports = router;
