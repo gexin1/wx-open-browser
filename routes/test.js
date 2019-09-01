56116cfd7d3be3c09d64fd0e21b8ddfede42a692
river <382599512@qq.com>
2019-09-01 21:12:19 +0800

    fix

--- routes/test.js
+++ routes/test.js
@@ -4,9 +4,9 @@ const path = require("path");
 // 流的方式下载
 router.get("/download", async (ctx, next) => {
   ctx.set({
-    "Content-Disposition": "attachment;filename=river.apk"
+    "Content-Disposition": "attachment;filename=1.png"
   });
-  await send(ctx, path.resolve("/public/assets/1.apk"));
+  await send(ctx, path.resolve("/public/assets/qrcode.png"));
 });
 
 module.exports = router;
